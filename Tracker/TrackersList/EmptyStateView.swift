fa7a88174c45be17572ce7d8fce5ea19157e0fda
GoldenSnake <elst1995@gmail.com>
2024-10-14 12:22:47 +0300

    Перенесен emtyStateView в отдельный файл

---
+++ Tracker/TrackersList/EmptyStateView.swift
@@ -0,0 +1,48 @@
+
+import UIKit
+
+class EmptyStateView: UIView{
+    
+    private let image = UIImageView()
+    private let label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(image)
+        setupImage()
+        addSubview(label)
+        setupLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupImage(){
+        image.image = UIImage(named: "Star")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            image.centerXAnchor.constraint(equalTo: centerXAnchor),
+            image.centerYAnchor.constraint(equalTo: centerYAnchor),
+            image.widthAnchor.constraint(equalToConstant: 80),
+            image.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    private func setupLabel(){
+        label.text = "Что будем отслеживать?"
+        label.textAlignment = .center
+        label.textColor = .ypBlack
+        label.font = .systemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 8),
+            label.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
+    
+}
